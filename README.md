48ad6a73b255ebb9e883b9c89e1d505a41f49f75
Muhammed Maral <muhammed.maral99@gmail.com>
2022-07-13 09:28:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Multi-Color-Task
+ 
---
+++ js/slider.js
@@ -0,0 +1,26 @@
+function goToTab(targetIndex) {
+  const navChildElems = document.querySelector("#navigation").children;
+  for (let i = 0; i < navChildElems.length; i++) {
+    navChildElems[i].classList.remove("active");
+  }
+  navChildElems[targetIndex].classList.add("active");
+
+  const galleryChildElems = document.querySelector(".slider__wrapper").children;
+  for (let i = 0; i < galleryChildElems.length; i++) {
+    galleryChildElems[i].classList.remove("slide-animation");
+    if (galleryChildElems[i].classList.contains("active") || i == targetIndex) {
+      galleryChildElems[i].classList.add("slide-animation");
+    }
+    galleryChildElems[i].classList.remove("active");
+    galleryChildElems[i].classList.remove("move-right");
+
+    if (targetIndex < i) {
+      galleryChildElems[i].classList.add("move-right");
+    }
+  }
+  galleryChildElems[targetIndex].classList.add("active");
+  document.querySelector(".slider__wrapper").style["min-height"] =
+    galleryChildElems[targetIndex].offsetHeight + "px";
+}
+
+goToTab(0);
